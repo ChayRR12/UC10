6e42a33d0a5d4a4d9103ff338307de22ea7414cb
ChayRR12 <chayelek@outlook.com>
2023-11-29 19:43:48 -0300

    Primeiro Commit

---
+++ src/uc10/UC10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uc10;
+
+/**
+ *
+ * @author 66211020764
+ */
+public class UC10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
